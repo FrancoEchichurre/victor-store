b6690ca98b1a5917e072d8cb08572b86338bec2f
Franco <francoechichurrez@gmail.com>
2025-10-24 00:35:59 -0300

    Cambio del header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic'
 
-const HomeContent = dynamic(() => import('@/components/home-content').then(mod => ({ default: mod.HomeContent })), {
+const HomeContent = dynamic(() => import('@/components/home').then(mod => ({ default: mod.HomeContent })), {
   ssr: false,
   loading: () => <div className="min-h-screen flex items-center justify-center">Cargando...</div>
 })
