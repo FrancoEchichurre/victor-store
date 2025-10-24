c26f7745b7e391c897f500153e1f49d20ccab064
Franco <francoechichurrez@gmail.com>
2025-10-23 23:19:59 -0300

    Fix: Dynamic import para home-content

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import { HomeContent } from "@/components/home-content"
+import dynamic from 'next/dynamic'
+
+const HomeContent = dynamic(() => import('@/components/home-content').then(mod => ({ default: mod.HomeContent })), {
+  ssr: false
+})
 
 export default function HomePage() {
   return <HomeContent />
